0d2838ea38dc0ac5a993e14fe9844216bbab3de2
Daybson B. Salles Paisante <daybson.paisante@outlook.com>
2015-05-15 15:32:19 -0300

    Initial commit

---
+++ Assets/Scripts/FloatingDamage.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FloatingDamage : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField]
+    private Text textDamage;
+    public Text TextDamage
+    {
+        get { return textDamage; }
+        set { textDamage = value; }
+    }
+
+    [SerializeField]
+    private float slideStep;
+
+    [SerializeField]
+    private float slideWaitTime;
+
+    [SerializeField]
+    [Range(0.1f, 0.9f)]
+    private float normalizedFadeStarts;
+
+    [SerializeField]
+    private float slideTotalTime;
+
+    private float timeFadeStarts;
+
+    #endregion
+
+    #region Methods
+
+    private void Awake()
+    {
+        this.textDamage.rectTransform.anchoredPosition =
+            this.textDamage.transform.parent.GetComponent<RectTransform>().anchoredPosition;
+    }
+
+    private void Update()
+    {
+        this.transform.position = Camera.main.WorldToScreenPoint(GameObject.Find("Avatar").transform.position);
+    }
+
+
+    public void ShowMe()
+    {
+        this.gameObject.SetActive(true);
+        StartCoroutine("SlideTop");
+        this.timeFadeStarts = this.slideTotalTime * this.normalizedFadeStarts;
+        Invoke("FadeOut", this.timeFadeStarts);
+    }
+
+
+    private void FadeOut()
+    {
+        this.textDamage.CrossFadeAlpha(0, this.slideTotalTime - this.timeFadeStarts, false);
+        Invoke("Respaw", this.slideTotalTime);
+    }
+
+    private void Respaw()
+    {
+        StopCoroutine("SlideTop");
+        this.gameObject.SetActive(false);
+        this.textDamage.rectTransform.anchoredPosition = Vector2.zero;
+        this.textDamage.CrossFadeAlpha(1, 0, true);
+    }
+
+    private IEnumerator SlideTop()
+    {
+        while (true)
+        {
+            this.textDamage.rectTransform.anchoredPosition = new Vector2(
+                this.textDamage.rectTransform.anchoredPosition.x,
+                this.textDamage.rectTransform.anchoredPosition.y + this.slideStep);
+
+            yield return new WaitForSeconds(this.slideWaitTime);
+        }
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/FloatingDamagePooler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class FloatingDamagePooler : MonoBehaviour
+{
+    private List<FloatingDamage> pooler = new List<FloatingDamage>();
+    private const int MAX_OBJ = 10;
+
+    [SerializeField]
+    private Canvas canvas;
+
+    public void CreateOrUseObj(string text)
+    {
+        FloatingDamage fd;
+
+        if (pooler.Count < MAX_OBJ)
+        {
+            var obj = (GameObject)GameObject.Instantiate(Resources.Load("Prefabs/FloatingDamage"));
+            obj.transform.SetParent(this.canvas.transform, false);
+
+            fd = obj.GetComponent<FloatingDamage>();
+            pooler.Add(fd);
+        }
+        else
+        {
+            if (pooler.Where(f => f.gameObject.activeSelf).Count() == MAX_OBJ)
+                return;
+
+            fd = pooler.Where(f => !f.gameObject.activeSelf).First().GetComponent<FloatingDamage>();
+            fd.gameObject.SetActive(true);
+        }
+
+        fd.transform.position = Camera.main.WorldToScreenPoint(this.transform.position);
+        fd.TextDamage.text = text;
+        fd.ShowMe();
+    }
+}
